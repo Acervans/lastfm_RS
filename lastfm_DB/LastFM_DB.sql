c612f48324314a9438a000ac3f5aec103f6eea79
Acervans <javierwang01052001@gmail.com>
2022-10-24 01:17:18 +0200

    Fix Diagram, Add SQL Script to create DDBB, Further develop data scraper

---
+++ lastfm_DB/LastFM_DB.sql
@@ -0,0 +1,171 @@
+CREATE TABLE IF NOT EXISTS Album (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name varchar NOT NULL,
+  artist_id integer NOT NULL,
+  VAD varchar,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS AlbumTopTags (
+  album_id integer NOT NULL,
+  tag_id integer NOT NULL,
+  PRIMARY KEY (album_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS Artist (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name varchar NOT NULL UNIQUE,
+  VAD varchar,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ArtistTopTags (
+  artist_id integer NOT NULL,
+  tag_id integer NOT NULL,
+  PRIMARY KEY (artist_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS Tag (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name varchar NOT NULL UNIQUE,
+  VAD varchar,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Track (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name varchar NOT NULL,
+  artist_id integer NOT NULL,
+  album_id integer,
+  VAD varchar,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS TrackTopTags (
+  track_id integer NOT NULL,
+  tag_id integer NOT NULL,
+  PRIMARY KEY (track_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS User_ (
+  id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+  username varchar NOT NULL UNIQUE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS UserLovedTracks (
+  user_id integer NOT NULL,
+  track_id integer NOT NULL,
+  love_at timestamp,
+  PRIMARY KEY (user_id, track_id)
+);
+
+CREATE TABLE IF NOT EXISTS UserRecentTracks (
+  user_id integer NOT NULL,
+  track_id integer NOT NULL,
+  listen_at timestamp,
+  PRIMARY KEY (user_id, track_id)
+);
+
+CREATE TABLE IF NOT EXISTS UserTopAlbums (
+  user_id integer NOT NULL,
+  album_id integer NOT NULL,
+  PRIMARY KEY (user_id, album_id)
+);
+
+CREATE TABLE IF NOT EXISTS UserTopArtists (
+  user_id integer NOT NULL,
+  artist_id integer NOT NULL,
+  PRIMARY KEY (user_id, artist_id)
+);
+
+CREATE TABLE IF NOT EXISTS UserTopTags (
+  user_id integer NOT NULL,
+  tag_id integer NOT NULL,
+  PRIMARY KEY (user_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS UserTopTracks (
+  user_id integer NOT NULL,
+  track_id integer NOT NULL,
+  PRIMARY KEY (user_id, track_id)
+);
+
+ALTER TABLE
+  UserLovedTracks
+ADD
+  CONSTRAINT FK_User_TO_UserLovedTracks FOREIGN KEY (user_id) REFERENCES User_ (id),
+ADD
+  CONSTRAINT FK_Track_TO_UserLovedTracks FOREIGN KEY (track_id) REFERENCES Track (id);
+
+ALTER TABLE
+  UserTopArtists
+ADD
+  CONSTRAINT FK_User_TO_UserTopArtists FOREIGN KEY (user_id) REFERENCES User_ (id),
+ADD
+  CONSTRAINT FK_Artist_TO_UserTopArtists FOREIGN KEY (artist_id) REFERENCES Artist (id);
+
+ALTER TABLE
+  UserTopAlbums
+ADD
+  CONSTRAINT FK_User_TO_UserTopAlbums FOREIGN KEY (user_id) REFERENCES User_ (id),
+ADD
+  CONSTRAINT FK_Album_TO_UserTopAlbums FOREIGN KEY (album_id) REFERENCES Album (id);
+
+ALTER TABLE
+  UserTopTags
+ADD
+  CONSTRAINT FK_User_TO_UserTopTags FOREIGN KEY (user_id) REFERENCES User_ (id),
+ADD
+  CONSTRAINT FK_Tag_TO_UserTopTags FOREIGN KEY (tag_id) REFERENCES Tag (id);
+
+ALTER TABLE
+  UserRecentTracks
+ADD
+  CONSTRAINT FK_User_TO_UserRecentTracks FOREIGN KEY (user_id) REFERENCES User_ (id),
+ADD
+  CONSTRAINT FK_Track_TO_UserRecentTracks FOREIGN KEY (track_id) REFERENCES Track (id);
+
+ALTER TABLE
+  UserTopTracks
+ADD
+  CONSTRAINT FK_User_TO_UserTopTracks FOREIGN KEY (user_id) REFERENCES User_ (id),
+ADD
+  CONSTRAINT FK_Track_TO_UserTopTracks FOREIGN KEY (track_id) REFERENCES Track (id);
+
+ALTER TABLE
+  Album
+ADD
+  CONSTRAINT FK_Artist_TO_Album FOREIGN KEY (artist_id) REFERENCES Artist (id),
+ADD
+  CONSTRAINT UQ_Album_Artist UNIQUE (name, artist_id);
+
+ALTER TABLE
+  Track
+ADD
+  CONSTRAINT FK_Artist_TO_Track FOREIGN KEY (artist_id) REFERENCES Artist (id),
+ADD
+  CONSTRAINT FK_Album_TO_Track FOREIGN KEY (album_id) REFERENCES Album (id),
+ADD
+  CONSTRAINT UQ_Track_Artist UNIQUE (name, artist_id);
+
+ALTER TABLE
+  ArtistTopTags
+ADD
+  CONSTRAINT FK_Artist_TO_ArtistTopTags FOREIGN KEY (artist_id) REFERENCES Artist (id),
+ADD
+  CONSTRAINT FK_Tag_TO_ArtistTopTags FOREIGN KEY (tag_id) REFERENCES Tag (id);
+
+ALTER TABLE
+  TrackTopTags
+ADD
+  CONSTRAINT FK_Tag_TO_TrackTopTags FOREIGN KEY (tag_id) REFERENCES Tag (id),
+ADD
+  CONSTRAINT FK_Track_TO_TrackTopTags FOREIGN KEY (track_id) REFERENCES Track (id);
+
+ALTER TABLE
+  AlbumTopTags
+ADD
+  CONSTRAINT FK_Tag_TO_AlbumTopTags FOREIGN KEY (tag_id) REFERENCES Tag (id),
+ADD
+  CONSTRAINT FK_Album_TO_AlbumTopTags FOREIGN KEY (album_id) REFERENCES Album (id);
--- lastfm_RS/backend/src/get_lastfm_data.py
+++ lastfm_RS/backend/src/get_lastfm_data.py
@@ -1,4 +1,5 @@
 from lyricsgenius import Genius
+# from nrc_vad_analysis import analyze_string, analyze_text
 from bs4 import BeautifulSoup
 import pylast
 import requests
@@ -17,13 +18,10 @@ payload = {"username_or_email": "Test_EPS", "password": "Tfg.EPS2022"}
 DATA_FOLDER = '../data'
 LOGIN_URL = "https://www.last.fm/login"
 
-# kb = pylast.Artist('Kudasaibeats', network)
-# for x in kb.get_top_tags():
-#     print(x.item.get_name())
-# print(kb.get_bio_summary())
-
-# # Maybe use some tags + bio summary to evaluate VAD of an artist
-# print(genius.search_song('lone digger', 'caravan palace').lyrics)
+TRACK_LIMIT = 1
+ARTIST_LIMIT = 1
+ALBUM_LIMIT = 1
+TAG_LIMIT = 1
 
 
 def login_lastfm(session):
@@ -62,7 +60,7 @@ def get_top_listeners(session, artist):
     return top_listeners
 
 
-def get_top_tags(limit):
+def get_chart_top_tags(limit):
     return [tag[0] for tag in network.get_top_tags(limit=limit)]
 
 
@@ -75,16 +73,31 @@ def get_tag_top_artists(tags):
     return tag_top_artists
 
 
+def get_track_info(track: pylast.Track):
+    artist = track.get_artist().get_name()
+    album = track.get_album()
+    if album:
+        album = album.get_name()
+    else:
+        album = ''
+    unique_track = (track.get_name(), artist, album)
+
+    # TODO get VAD?
+
+    return unique_track
+
+
 if __name__ == "__main__":
 
-    available_opts = ('-l', '-d', '-a')
+    available_opts = ('-l', '-d', '-t', '-a')
 
     if len(sys.argv) != 2 or sys.argv[1] not in available_opts:
         print(f'Usage: python3 {sys.argv[0]} [Option]')
         print('Options:')
         print('\t-l => Scrapes listeners from top listeners, obtained from top tags')
         print('\t-d => Obtains data from listeners saved with -l')
-        print('\t-a => Scrapes listeners and obtains data directly')
+        print('\t-t => Obtains top tags for all the items stored with -d')
+        print('\t-a => Does everything. Scrapes listeners and obtains data and tags')
         sys.exit()
 
     if sys.argv[1] in ('-l', '-a'):
@@ -95,7 +108,7 @@ if __name__ == "__main__":
 
             print('Getting top 10 tags...')
             # Get top 10 tags
-            chart_tags = get_top_tags(limit=10)
+            chart_tags = get_chart_top_tags(limit=10)
             print(f"Tags: {[tag.name for tag in chart_tags]}", end='\n\n')
 
             print('Getting top artists...')
@@ -134,14 +147,144 @@ if __name__ == "__main__":
                 for l in unique_listeners:
                     f.write(f"{l}\n")
 
+    unique_tags = set()
+
     if sys.argv[1] in ('-d', '-a'):
-        # Get loved/recent/top tracks, top tags, top artists, top albums
+
+        # Get loved/recent/top tracks, top artists, top albums
+        # NOTE: Search tags of each item AFTER storing everything
+        # tags = [tag[0].get_name()
+        #     for tag in x.get_top_tags(limit=TAG_LIMIT)]
+
+        # unique_tags.update(tags)
+        #             item_tags[unique_track] = tags
+        # TODO: VAD
+        # kb = pylast.Artist('Kudasaibeats', network)
+        # for x in kb.get_top_tags():
+        #     print(x.item.get_name())
+        # print(kb.get_bio_summary())
+
+        # # Maybe use some tags + bio summary to evaluate VAD of an artist
+        # print(genius.search_song('lone digger', 'caravan palace').lyrics)
+
         unique_artists = set()
         unique_albums = set()
         unique_tracks = set()
+        loved_tracks = dict()
+        recent_tracks = dict()
+        top_tracks = dict()
+        top_artists = dict()
+        top_albums = dict()
+
         with open(f'{DATA_FOLDER}/all_unique_listeners.dat', 'r') as f:
             print('Getting data from all unique listeners...')
-            for i, listener in enumerate(f.readlines()):
-                # user = network.get_user(listener)
-                print(f'[{i}] {listener[:-1]}')
-                pass
+            for i, listener in enumerate(f.readlines()[:1]):
+                listener = listener.strip()
+
+                print(f'[{i}] {listener}')
+                user = network.get_user(listener)
+
+                # ---------------------- Loved Tracks ----------------------
+                print('\t- Getting loved tracks...')
+                lt = user.get_loved_tracks(limit=TRACK_LIMIT)
+                loved_tracks[listener] = list()
+                for t in lt:
+                    track = t[0]
+                    unique_track = get_track_info(track)
+                    track_name, artist, album = unique_track
+
+                    if album:
+                        unique_albums.add('\u254E'.join([album, artist]))
+                    unique_artists.add(artist)
+                    unique_tracks.add('\u254E'.join(unique_track))
+
+                    loved_tracks[listener].append(
+                        '\u254E'.join([track_name, str(t[-1])]))
+
+                # ---------------------- Recent Tracks ----------------------
+                print('\t- Getting recent tracks...')
+                rt = user.get_recent_tracks(limit=TRACK_LIMIT)
+                recent_tracks[listener] = list()
+                for t in rt:
+                    track = t[0]
+                    unique_track = get_track_info(track)
+                    track_name, artist, album = unique_track
+
+                    if album:
+                        unique_albums.add('\u254E'.join([album, artist]))
+                    unique_artists.add(artist)
+                    unique_tracks.add('\u254E'.join(unique_track))
+
+                    recent_tracks[listener].append(
+                        '\u254E'.join([track_name, str(t[-1])]))
+
+                # ---------------------- Top Tracks ----------------------
+                print('\t- Getting top tracks...')
+                tt = user.get_top_tracks(limit=TRACK_LIMIT)
+                top_tracks[listener] = list()
+                for t in tt:
+                    track = t[0]
+                    unique_track = get_track_info(track)
+                    track_name, artist, album = unique_track
+
+                    if album:
+                        unique_albums.add('\u254E'.join([album, artist]))
+                    unique_artists.add(artist)
+                    unique_tracks.add('\u254E'.join(unique_track))
+
+                    top_tracks[listener].append(track_name)
+
+                # ---------------------- Artists ----------------------
+                print('\t- Getting top artists...')
+                top_artists[listener] = list()
+                for artist in user.get_top_artists(limit=ARTIST_LIMIT):
+                    artist = artist[0]
+                    artist_name = artist.get_name()
+                    unique_artists.add(artist_name)
+                    top_artists[listener].append(artist_name)
+                    # TODO get VAD?
+
+                # ---------------------- Albums ----------------------
+                print('\t- Getting top albums...')
+                top_albums[listener] = list()
+                for album in user.get_top_albums(limit=ALBUM_LIMIT):
+                    album = album[0]
+                    artist_name = album.get_artist().get_name()
+                    album_name = album.get_name()
+                    unique_albums.add('\u254E'.join([album_name, artist_name]))
+                    top_albums[listener].append(
+                        '\u254E'.join([album_name, artist_name]))
+                    # TODO get VAD?
+
+        with open(f'{DATA_FOLDER}/loved_tracks.json', 'w') as f:
+            f.write(json.dumps(loved_tracks,
+                    indent=4, ensure_ascii=False))
+
+        with open(f'{DATA_FOLDER}/recent_tracks.json', 'w') as f:
+            f.write(json.dumps(recent_tracks,
+                    indent=4, ensure_ascii=False))
+
+        with open(f'{DATA_FOLDER}/top_tracks.json', 'w') as f:
+            f.write(json.dumps(top_tracks, indent=4, ensure_ascii=False))
+
+        with open(f'{DATA_FOLDER}/top_artists.json', 'w') as f:
+            f.write(json.dumps(top_artists, indent=4, ensure_ascii=False))
+
+        with open(f'{DATA_FOLDER}/top_albums.json', 'w') as f:
+            f.write(json.dumps(top_albums, indent=4, ensure_ascii=False))
+
+        with open(f'{DATA_FOLDER}/unique_tracks.dat', 'w') as f:
+            for track in unique_tracks:
+                f.write(f"{track}\n")
+
+        with open(f'{DATA_FOLDER}/unique_artists.dat', 'w') as f:
+            for artist in unique_artists:
+                f.write(f"{artist}\n")
+
+        with open(f'{DATA_FOLDER}/unique_albums.dat', 'w') as f:
+            for album in unique_albums:
+                f.write(f"{album}\n")
+
+    if sys.argv[1] in ('-t', '-a'):
+        # Get top tags for items (artists, albums, tracks)
+        item_tags = dict()
