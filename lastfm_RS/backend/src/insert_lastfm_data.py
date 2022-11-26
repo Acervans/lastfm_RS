823d2c0b00174933d8c1757c7d84a848ad0d1dbc
Acervans <javierwang01052001@gmail.com>
2022-11-26 01:43:56 +0100

    Move SQL files to backend

--- lastfm_RS/backend/src/insert_lastfm_data.py
+++ lastfm_RS/backend/src/insert_lastfm_data.py
@@ -1,7 +1,38 @@
-import sqlalchemy
+import sqlalchemy as sa
 
-db = sqlalchemy.create_engine("postgresql://alumnodb:alumnodb@localhost:5432/lastfm_db")
-conn = db.connect()
+DATA_FOLDER = '../data/lastfm_data'
+SQL_FOLDER = '../sql'
+
+db = sa.create_engine("postgresql://alumnodb:alumnodb@localhost:5432/lastfm_db", echo=True)
+
+def table_exists(engine, name):
+    ins = sa.inspect(engine)
+    return ins.dialect.has_table(engine.connect(), name)
+
+def setup_db(engine):
+    with engine.connect() as conn:
+        with open(f"{SQL_FOLDER}/LastFM_DB.sql") as f:
+            query = sa.text(f.read())
+            conn.execute(query)
 
 def insert_tags():
     pass
+
+def insert_artists():
+    pass
+
+def insert_albums():
+    pass
+
+def insert_tracks():
+    pass
+
+def insert_users():
+    pass
+
+if __name__ == "__main__":
+    # Create tables and relations using SQL script
+    if not table_exists(db, 'track'):
+        setup_db(db)
+        
+    insert_tags()
