79e94e4ba6b214156627c05ef9f40b823c8dfb33
Javier Wang Zhou <javier.wang@estudiante.uam.es>
2022-12-03 20:39:21 +0100

    Handle Suspicious AssertionError

--- lastfm_RS/backend/src/get_lastfm_data.py
+++ lastfm_RS/backend/src/get_lastfm_data.py
@@ -537,7 +537,7 @@ if __name__ == "__main__":
                         print_load_percentage(i+1, total_tags)
                         break
 
-                    except (ConnectionError, ReadTimeout):
+                    except (ConnectionError, AssertionError, ReadTimeout):
                         attempts += 1
                     except WikipediaException:
                         continue
