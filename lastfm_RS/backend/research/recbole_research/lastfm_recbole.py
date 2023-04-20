e12a6bf9c54edd63c09a1863faa5bc0b39e85630
Javier Wang Zhou <javier.wang@estudiante.uam.es>
2023-04-21 00:25:48 +0200

    Custom xDeepFM model implementing VADSt

--- lastfm_RS/backend/research/recbole_research/lastfm_recbole.py
+++ lastfm_RS/backend/research/recbole_research/lastfm_recbole.py
@@ -1,84 +1,55 @@
+from recbole.model.abstract_recommender import ContextAwareMixin
+from recbole.model.context_aware_recommender import xDeepFM
+
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
 
-from recbole.utils import InputType
-from recbole.model.abstract_recommender import ContextRecommender
-from recbole.model.context_aware_recommender import fm
-from recbole.model.loss import BPRLoss
-from recbole.model.init import xavier_normal_initialization
-
-from logging import getLogger
-from recbole.utils import init_logger, init_seed
-from recbole.trainer import Trainer
-from recbole.config import Config
-from recbole.data import create_dataset, data_preparation
-from sys import argv
-
-import numpy as np
-import pandas as pd
-
-class LastRS(fm.FM):
-
-    input_type = InputType.PAIRWISE
-
+class xDeepFMContextAware(ContextAwareMixin, xDeepFM):
+    
     def __init__(self, config, dataset):
         super().__init__(config, dataset)
-
-    def calculate_loss(self, interaction):
-        super().calculate_loss(interaction)
-
+        
+        self.vadst_embedding = torch.nn.Embedding(
+            dataset.item_vadst_feat.field_size['vadst'], 
+            config.embedding_size, 
+            padding_idx=0
+        )
+        
+    def forward(self, interaction):
+        vadst = interaction[self.ITEM_FEAT_FIELDS]['vadst']   # (batch_size, field_size)
+        vadst_emb = self.vadst_embedding(vadst)   # (batch_size, field_size, embedding_size)
+        
+        embedding_dict = self.get_embedding_dict(interaction)
+        embedding_list = self.embedding_list(embedding_dict)
+        x_deep = self.xDeep(embedding_list)
+        xfm = self.fm(embedding_list)
+        
+        x = torch.cat([x_deep, xfm], dim=1)
+        x = self.mlp(x)
+        x = self.predict_layer(x)
+        
+        return x, vadst_emb
+    
     def predict(self, interaction):
+        scores, vadst_emb = self.forward(interaction)
         user = interaction[self.USER_ID]
-        item = interaction[self.ITEM_ID]
-
-        user_e = self.user_embedding(user)            # [batch_size, embedding_size]
-        item_e = self.item_embedding(item)            # [batch_size, embedding_size]
-
-        scores = torch.mul(user_e, item_e).sum(dim=1) # [batch_size]
-
+        pos_items = interaction[self.POS_ITEM_ID]
+        pos_ratings = interaction[self.POS_ITEM_RATINGS]
+        pos_vadst = vadst_emb[:, self.item2id(pos_items), :]   # (batch_size, num_pos_items, embedding_size)
+        dist = F.pairwise_distance(vadst_emb.unsqueeze(1), pos_vadst, p=2)   # (batch_size, num_pos_items, field_size)
+        weights = 1.0 / (1.0 + dist)
+        scores = (scores * weights * pos_ratings.unsqueeze(1)).sum(dim=1)
+        
         return scores
-
+    
     def full_sort_predict(self, interaction):
+        scores, vadst_emb = self.forward(interaction)
         user = interaction[self.USER_ID]
-
-        user_e = self.user_embedding(user)                        # [batch_size, embedding_size]
-        all_item_e = self.item_embedding.weight                   # [n_items, batch_size]
-
-        scores = torch.matmul(user_e, all_item_e.transpose(0, 1)) # [batch_size, n_items]
-
+        pos_items = interaction[self.POS_ITEM_ID]
+        pos_ratings = interaction[self.POS_ITEM_RATINGS]
+        pos_vadst = vadst_emb[:, self.item2id(pos_items), :]   # (batch_size, num_pos_items, embedding_size)
+        dist = F.pairwise_distance(vadst_emb.unsqueeze(1), pos_vadst, p=2)   # (batch_size, num_pos_items, field_size)
+        weights = 1.0 / (1.0 + dist)
+        scores = (scores * weights * pos_ratings.unsqueeze(1)).sum(dim=1)
+        
         return scores
-
-
-if __name__ == '__main__':
-
-    config = Config(model=LastRS, config_file_list=[argv[1]])
-    init_seed(config['seed'], config['reproducibility'])
-
-    # logger initialization
-    init_logger(config)
-    logger = getLogger()
-
-    logger.info(config)
-
-    # dataset filtering
-    dataset = create_dataset(config)
-    logger.info(dataset)
-
-    # dataset splitting
-    train_data, valid_data, test_data = data_preparation(config, dataset)
-
-    # model loading and initialization
-    model = LastRS(config, train_data.dataset).to(config['device'])
-    logger.info(model)
-
-    # trainer loading and initialization
-    trainer = Trainer(config, model)
-
-    # model training
-    best_valid_score, best_valid_result = trainer.fit(train_data, valid_data)
-
-    # model evaluation
-    test_result = trainer.evaluate(test_data)
-
-    logger.info('Best valid result: {}'.format(best_valid_result))
-    logger.info('Test result: {}'.format(test_result))
